a3ff03f0dfe1648df4d9d8de240da33870fdd320
pablo <pablo@pablo-ThinkPad-T430>
2020-04-19 21:25:11 -0300

    Version Inicial

--- src/ar/edu/unq/doc/tp3/Punto.java
+++ src/ar/edu/unq/doc/tp3/Punto.java
@@ -5,8 +5,11 @@ public class Punto {
 	private float y;
 	public Punto(float x, float y) {
 		super();
-		this.x = x;
-		this.y = y;
+		this.setXY(x, y);
+	}
+	public void setXY(float x, float y) {
+		this.setX(x);
+		this.setY(y);
 	}
 	public float getX() {
 		return x;
