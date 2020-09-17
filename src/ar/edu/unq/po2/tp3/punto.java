44627a4343385e1da2a0fc3b972fcff102d4cabf
Pablo Bernasconi <pablonicolas17@gmail.com>
2020-09-16 23:27:35 -0300

    Version Inicial

---
+++ src/ar/edu/unq/po2/tp3/punto.java
@@ -0,0 +1,30 @@
+package ar.edu.unq.po2.tp3;
+
+public class punto {
+	
+	private float x;
+	private float y;
+	
+	public punto(float x, float y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+}
